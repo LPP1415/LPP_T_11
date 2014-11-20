70d7619b5a9fb2aa6937b20956efcf50c363d2b8
Eduardo Jesús Díaz Dioniz <eddioniz@gmail.com>
2014-11-20 13:05:59 +0000

    ADD all change interfaz

--- lib/exam/examen.rb
+++ lib/exam/examen.rb
@@ -1,44 +1,81 @@
+if __FILE__ == $0 then
+    $: << "."
+end
+
 require './nodelist/list'
 require './question/simpleChoice/base'
 require './question/trueOrFalse/base'
 require './question/questionFather/base'
 
 class Examen
-	@@q1 = SimpleChoice.new(:text => '2 + 2 = ',:right => '4',:distractor => [5,3,2], :dif =>1)
-	@@q2 = SimpleChoice.new(:text => '2 * 2 = ',:right => '4',:distractor => [5,3,2], :dif =>2)
-	@@q3 = SimpleChoice.new(:text => '3 + 2 = ',:right => '5',:distractor => [6,3,2], :dif =>3)
-	@@q4 = SimpleChoice.new(:text => '2 - 1 = ',:right => '1',:distractor => [0,3,2], :dif =>4)
-	@@q5 = SimpleChoice.new(:text => '2 - 2 = ',:right => '0',:distractor => [4,3,2], :dif =>5)
-	@@q6 = TrueOrFalse.new(:text => '2 + 2 = 4',:right => 'true', :dif =>6)
-	@@q7 = TrueOrFalse.new(:text => '2 * 2 = 4',:right => 'true', :dif =>7)
-	@@q8 = TrueOrFalse.new(:text => '3 + 2 = 6',:right => 'false', :dif =>8)
-	@@q9 = TrueOrFalse.new(:text => '2 - 2 = 1',:right => 'false', :dif =>9)
-	@@q10 = TrueOrFalse.new(:text => '2 - 1 = 1',:right => 'true', :dif =>10)
+    attr_accessor :list , :node_i , :acierto , :fallo
+    def initialize(list)
+        @acierto = 0
+        @fallo = 0
+        @list = list
+    end
 
-	@@list = Lista.new()
-	@@list.add(@@q1,@@q2,@@q3,@@q4,@@q5,@@q6,@@q7,@@q8,@@q9,@@q10)
-	@@evaluacion = Array.new()
-	@@n = 1
-	@@aciertos = 0
-	@@fallos = 0
+    def next_question()
+        if @node_i != nil then
+            if @node_i.next != nil then
+                @node_i = @node_i.next
+            else
+                return nil
+            end
+        else
+            @node_i = @list.head
+        end
+        puts @node_i.value.to_s
+        puts "-----v"
+        return @node_i.value.to_s
+    end
+    def response_question(value)
+        print "Respuesta: "
+        if value == @node_i.value.right then
+            puts "\n"
+            puts "La pregunta es correcta"
+            puts "\n"
+            @acierto += 1
+        else
+            puts "\n"
+            puts "La pregunta es incorrecta"
+            puts "\n"
+            @fallo += 1
+        end
+    end
+    def show_stats
+        puts "\n"
+        puts "|---------------------------|"
+        puts "|---     Estadisticas    ---|"
+        puts "|---------------------------|"
+        puts "|- Aciertos: #{@acierto} | Fallos: #{@fallo} -|"
+        puts "|---------------------------|"
+        puts "\n"
+    end
+end
+if $0 == __FILE__ then
 
-	@@list.each do |q|
-		print "#{@@n})"		 
-		puts q.to_s
-		print "Respuesta: "
-		STDOUT.flush
-		res = gets.chomp
-		if res == q.right then
-			@@evaluacion.push("La pregunta #{@@n} es correcta")
-			@@aciertos = @@aciertos + 1
-		else
-			@@evaluacion.push("La pregunta #{@@n} es incorrecta")
-			@@fallos = @@fallos + 1
-		end
-		@@n = @@n + 1
-	end
-	puts "-----------------EVALUACION------------------------"
-	puts "Fallos : #{@@fallos}, Aciertos: #{@@aciertos}"
-	@@evaluacion.each {|x| puts x}
-	puts "---------------------------------------------------"
+    p1 = SimpleChoice.new(:text => '1)2 + 2 = ',:right => '1',:distractor => [5,3,2], :dif =>1)
+    p2 = SimpleChoice.new(:text => '2)5 + 2 = ',:right => '2',:distractor => [5,3,2], :dif =>1)
+    p3 = SimpleChoice.new(:text => '3)2 * 2 = ',:right => '3',:distractor => [5,3,2], :dif =>1)
+
+    list = Lista.new()
+    list.add(p1,p2,p3)
+    examen = Examen.new(list)
+
+    examen.next_question
+    STDOUT.flush
+    value = gets.chomp
+    examen.response_question(value)
+    examen.next_question
+    STDOUT.flush
+    value = gets.chomp
+    examen.response_question(value)
+    examen.next_question
+    STDOUT.flush
+    value = gets.chomp
+    examen.response_question(value)
+
+    examen.show_stats
+
 end
